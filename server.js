27fb2afe2e589528309dbe54b7240ae3bfce0f2b
root <root@nubes.dtla.skysilk.net>
2018-08-01 13:52:58 +0000

    First Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+/*
+*	Poke - Multi Service / Domail Proxy
+*
+*/
+
+
+console.log('Starting to Poke');
+
+// Setup Dependencies
+express = require('express');
+
+// Get the services table
+const services = require('./data/services.json');
+
+// Setup main proxy
+const proxy = '';
+
+
+
+
+console.log('Ready to Catch them all');
