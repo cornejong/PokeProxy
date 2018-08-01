9c7c2e35c149d2065850a056a1edb57f76b10870
Corne de Jong <corne.dejong@me.com>
2018-08-01 22:08:56 +0200

    First Aplha working version

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const httpProxy = require('http-proxy');
 const fs = require('fs');
 
 // Load Services object
-var serviceTable = fs.readFileSync('./data/services.json');
+var serviceTable = fs.readFileSync('data/services.json');
 
 // Setup main proxy
 const proxy = {
@@ -22,34 +22,15 @@ const proxy = {
     services: JSON.parse(serviceTable)
 };
 
-
-/* TEMP */
-
-// TODO!
-// For all of proxy.services
-// new httpProxy.createProxyServer
-// Store object in proxy.services.[].proxy
-
 // Create a proxy for each service and store it with the asoc data
 proxy.services.forEach((service, index)  => {
-    proxy.services.index.porxy = new httpProxy.createProxyServer({
+    proxy.services[index].proxy = new httpProxy.createProxyServer({
         target: service.target
     });
 });
 
-/* var proxy_web = new httpProxy.createProxyServer({
-        target: {
-            host: 'localhost',
-            port: 8080
-        }
-    }); */
 
 http.createServer((req, res) => {
-
-    // TODO!
-        // For all of proxy.services
-        // if req.header.host === proxy.services.host
-            // do stuff...
 
     proxy.services.forEach((service, index) => {
         if(req.headers.host === service.host) {
@@ -62,23 +43,6 @@ http.createServer((req, res) => {
         }
     });
 
-/*         if (req.headers.host === 'http://www.domain.com') {
-            proxy_web.proxyRequest(req, res);
-            proxy_web.on('error', function(err, req, res) {
-                if (err) console.log(err);
-                res.writeHead(500);
-                res.end('Oops, something went very wrong...');
-            });
-        } else if (req.headers.host === 'http://api.domain.com') {
-            proxy_api.proxyRequest(req, res);
-            proxy_api.on('error', function(err, req, res) {
-                if (err) console.log(err);
-                res.writeHead(500);
-                res.end('Oops, something went very wrong...');
-            });
-        } */
 }).listen(proxy.port);
 
-/* END TEMP */
-
-console.log('Ready to Catch them all');
+console.log('Ready to Catch them all!');
